7e235bee7e8c0eae42d8cda10ca8c3c1dd220e79
Oliver Lade <piemaster21@gmail.com>
2014-08-12 19:04:46 +1000

    Moving toward Docker logic.

--- host/server/rest.py
+++ host/server/rest.py
@@ -1,7 +1,19 @@
 from host.server.comm import ThriftClient
+from host.server.registry import Registry
 
 
 class RestServer:
+
+    def __init__(self):
+        self.registry = Registry()
+
+    def register(self, service, image_id):
+        # TODO(orlade): Get ServiceClass from image_id.
+        self.registry.register(service, image_id)
+
+    def unregister(self, service):
+        self.registry.unregister(service)
+
     def invoke(self, service, request):
         client = ThriftClient(service)
         response = client.send(service, request)
