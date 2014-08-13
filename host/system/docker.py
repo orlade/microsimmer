5d73a96f1d378620a6e24cad7886b62c3ad5410e
Oliver Lade <piemater21@gmail.com>
2014-08-14 00:33:33 +1000

    Fixed some bugs with thrift compilation.

--- host/system/docker.py
+++ host/system/docker.py
@@ -3,6 +3,7 @@ Contains classes wrapping various Docker functionality at the core of Computome.
 """
 
 import subprocess
+import os
 
 from host.system.constants import PACKAGE_ROOT
 
@@ -20,12 +21,12 @@ class Container:
         self.image = image
         self.package = package
 
-    @classmethod
-    def _execute(cls, arguments):
+    @staticmethod
+    def _execute(arguments):
         """
         Executes Docker with the given arguments.
 
-        :param arguments: The arguments to follow "sudo docker" on the command line.
+        :param arguments: A list of the arguments to follow "sudo docker" on the command line.
         :return: The response provided by Docker.
         """
         # TODO(orlade): Allow return of container stdout.
@@ -64,17 +65,36 @@ class Container:
 class ComputomeContainer(Container):
     def compile_thrift(self, host_dir=PACKAGE_ROOT):
         """
-        Executes the Docker command necessary to compile the image's Thrift specification into the given directory on
-        the host machine.
+        Compiles the image's Thrift specification into the given directory on the host machine. Involves copying the API
+        specification out of the container to be compiled on the host (assuming responsibility for the Thrift
+        dependency).
 
-        :param host_dir: The directoy on the host machine in which Thrift's generated "*-gen" directories will be
-        compiled.
+        :param host_dir: The directory on the host machine in which Thrift's generated directories will be compiled.
         """
-        output_dir = '/_output'
-        api_dir = '/api'
-        mount = '%s:%s' % (host_dir, output_dir)
-        command = 'thrift --gen py -o %s/%s %s/services.thrift' % (output_dir, self.package, api_dir)
-        self.run(command, volume_arg=mount)
+        # The location that the model API files should be stored.
+        api_dir_name = 'api'
+        api_dir = '/%s' % api_dir_name
+
+        # The location to mount to in the container.
+        mount_dir = '/_mount'
+
+        # The location to copy the API files into for compilation.
+        package_dir = os.path.join(host_dir, self.package)
+        if not os.path.isdir(package_dir):
+            os.mkdir(package_dir)
+
+        # The Docker mount (volume) argument.
+        mount = '%s:%s' % (package_dir, mount_dir)
+
+        # Copy the API files out.
+        self.run('cp -r %s %s' % (api_dir, mount_dir), volume_arg=mount)
+
+        # Path to the copied services.thrift IDL file.
+        thrift_path = '%s/%s/services.thrift' % (package_dir, api_dir_name)
+        # Compile the Thrift IDL.
+        thrift_command = 'thrift --gen py -o %s %s' % (package_dir, thrift_path)
+        print('Executing: %s' % thrift_command)
+        subprocess.call(thrift_command.split(' '))
 
 
 def image_to_package_name(image_id):
