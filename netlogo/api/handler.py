9e96364e4aec7b4213c36a52b44fdd77b5329175
Oliver Lade <piemater21@gmail.com>
2014-10-11 17:56:34 +1100

    Added stubs for NetLogo setup.

---
+++ netlogo/api/handler.py
@@ -0,0 +1,5 @@
+# Invokes NetLogo models.
+# See http://ccl.northwestern.edu/netlogo/docs/behaviorspace.html
+
+# subprocess.call('./netlogo-headless.sh --model models/Sample\ Models/Biology/Evolution/Altruism.nlogo --setup-file ~/netlogo-experiments/test-exp.xml --experiment TestExperiment --table ~/netlogo-experiments/test-exp.out')
+
